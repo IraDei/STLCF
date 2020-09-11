4f7a9c612bdc03bbdad275aa45217f68d010d22e
IraDei <33973471+IraDei@users.noreply.github.com>
2020-09-11 16:57:56 +0800

    Add files via upload

---
+++ img2video.m
@@ -0,0 +1,27 @@
+function [aviobj] = img2video(img_seq, nfrm, dir_opt, aviname, ext, frame_rate)
+%IMG2VIDEO 
+   
+    avi_dir = [dir_opt aviname];
+    aviobj = VideoWriter([avi_dir ext]);
+    aviobj.FrameRate = frame_rate;
+    open(aviobj);
+    for i=1:nfrm
+        if ~isempty(aviobj.Height)
+            if size(img_seq(:,:,i),1) ~= aviobj.Height || size(img_seq(:,:,i),2) ~= aviobj.Width
+                close(aviobj);
+                delete( aviname )
+                error('所有图片的尺寸要相同！！');
+            end
+        end
+        
+        %fprintf('frame %g: min/max = %g/%g\n', i, min(img_seq(:,:,i),[],'all'), max(img_seq(:,:,i),[],'all'));
+        writeVideo(aviobj, img_seq(:,:,i));
+%         try
+%             writeVideo(aviobj, img_seq(:,:,i));
+%         catch
+%             fprintf('error at writing frame %g.\n', i);
+%         end
+    end
+    close(aviobj);
+end
+
